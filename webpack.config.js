2408de60aff7373550a81ac78b2f7c8d7b5f4880
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-03-30 17:15:06 +0100

    Added plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: { path: path.resolve(__dirname, 'dist'), filename: 'js/bundle.js' },
   devServer: {
     contentBase: './dist'
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
