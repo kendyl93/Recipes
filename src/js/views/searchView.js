73b4e50bba588818b35291a70422974eb2653597
kendyl93 <p.stanecki93@gmail.com>
2019-04-04 19:44:37 +0200

    Added agorithm to limit the words of the recipe title

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,8 +12,27 @@ export const clearSearchRecipesList = () => {
   searchRecipesList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+  const limitable = title.length > limit;
+  const newTitle = [];
+
+  if (limitable) {
+    title.split(' ').reduce((acc, currentTitle) => {
+      if (acc + currentTitle.length <= limit) {
+        newTitle.push(currentTitle);
+      }
+      return acc + currentTitle.length;
+    }, 0);
+
+    return `${newTitle.join(' ')}...`;
+  }
+
+  return title;
+};
+
 const renderRecipe = recipe => {
   const { recipe_id, image_url, title, publisher } = recipe;
+  const shortenTitle = limitRecipeTitle(title);
 
   const markup = `
     <li>
@@ -22,7 +41,7 @@ const renderRecipe = recipe => {
                 <img src="${image_url}" alt="Test">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${title}</h4>
+                <h4 class="results__name">${shortenTitle}</h4>
                 <p class="results__author">${publisher}</p>
             </div>
         </a>
